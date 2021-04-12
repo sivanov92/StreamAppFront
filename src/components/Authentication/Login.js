a5bf8f2b47f90a7daf7adbb06e36c3d5c56de98f
Stanislav Ivanov <stanislav_ivanov9213@abv.bg>
2021-04-13 01:16:51 +0300

    login/register tests

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,8 +1,36 @@
+import {base_api_route , jwt_api_secret_key} from '.../config.js'
+var jwt = require('jsonwebtoken')
 
 const Login = () => {
+    jwt.sign({ foo: 'bar' }, jwt_api_secret_key, { algorithm: 'RS256' }, function(err, token) {
+        var jwt_token = token
+      });
+
+    let login_data = {
+        'email':'email',
+        'password':'password'
+    } // CHANGE VALUES LATER
+
+    const loginUser = async()=>{
+       let response = await fetch(base_api_route+'/login',{
+           method:'POST',
+           mode:'cors',
+           headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt_token}` 
+           },
+           data:JSON.stringify(login_data)
+       })
+       if(response.ok){
+        let res_data = await response.json()
+        // LOGIN LOGIC
+       }else{
+           return false
+       }
+    }
     return (
         <div  id='LoginContainer'>
-            <form className='form'>
+            <form className='form' onSubmit={loginUser}>
                 <div className='form-elements'>
                 <label for="emailField">Email</label>
                 <input type='text' name='email' id='emailField'/>
--- src/components/Authentication/Register.js
+++ src/components/Authentication/Register.js
@@ -1,8 +1,39 @@
+import {base_api_route , jwt_api_secret_key} from '.../config.js'
+var jwt = require('jsonwebtoken')
 
 const Register = () => {
+    jwt.sign({ foo: 'bar' }, jwt_api_secret_key, { algorithm: 'RS256' }, function(err, token) {
+        var jwt_token = token
+      });
+
+    let login_data = {
+        'firstName':'firstName',
+        'lastName':'lastName',
+        'email':'email',
+        'password':'password'
+    } // CHANGE VALUES LATER
+
+    const registerUser = async()=>{
+       let response = await fetch(base_api_route+'/register',{
+           method:'POST',
+           mode:'cors',
+           headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt_token}` 
+           },
+           data:JSON.stringify(login_data)
+       })
+       if(response.ok){
+        let res_data = await response.json()
+        // Register LOGIC
+       }else{
+           return false
+       }
+    }
+
     return (
         <div id='RegisterContainer' >
-            <form className='form'>
+            <form className='form' onSubmit={registerUser}>
                 <div className='form-elements'>
                 <label for="firstName">First name :</label>
                 <input type='text' name='firstName' id='firstName'/>
